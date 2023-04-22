486556519f8f26ace298f8ff2ffd1348e3a18b14
aashpaash <115119317+aashpaash@users.noreply.github.com>
2023-04-22 14:27:38 -0700

    modified index.html and script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var apiKey = "API_KEY";
-// "apiKey"= "4fb00fc4e626764db50e2ea3b5fe5a50",
-// url: "https://api.openweathermap.org/data/2.5/forecast?q=?&&units=metricappid=4fb00fc4e626764db50e2ea3b5fe5a50"
+"apiKey"= "4fb00fc4e626764db50e2ea3b5fe5a50",
+url = "https://api.openweathermap.org/data/2.5/forecast?q=?&&units=metricappid=4fb00fc4e626764db50e2ea3b5fe5a50"
 var city = "Paris, France";
 
 //Fetch Weather
@@ -26,10 +26,10 @@ var weather = {
     };
 
     function displayWeather({
-        temp: forecast.main.temp,
-        humidity: forecast.main.humidity,
-        windSpeed: forecast.wind.speed,
-        icon: forecast.weather[0].icon
+        temp: forecast,main,temp,
+        humidity: forecast,main,humidity,
+        windSpeed: forecast,wind,speed,
+        icon: forecast,weather,[0]:icon
     }) 
     
     {

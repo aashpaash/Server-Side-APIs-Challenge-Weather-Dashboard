c66ac10db0ad6b8d9b4cd25f1d6b60d05d22e444
aashpaash <115119317+aashpaash@users.noreply.github.com>
2023-05-08 20:06:11 -0700

    improved script js edited to fix errors

--- script.js
+++ script.js
@@ -2,83 +2,47 @@
 // API data from Open Weather Map
 var APIKey = "4fb00fc4e626764db50e2ea3b5fe5a50";
 var queryURL = "https://api.openweathermap.org/data/2.5/forecast";
-var inputValue = input.value;
 var city;
 
-
-
 // Weather data Document Object Model - DOM
-var button = document.querySelector('.search-bar')
-var weather = document.querySelector('.weather')
-var form = document.querySelector('.input.search-bar')
+var button = document.querySelector('.search-bar');
+var weather = document.querySelector('.weather');
+var form = document.querySelector('.input.search-bar');
 
 // Fetch Weather from Requested City
-
-//fetch(queryURL)
+function getWeather(inputValue) {
+fetch(https://api.openweathermap.org/data/2.5/forecast?q=${inputValue}&appid=${APIKey})
+.then(response => response.json())
+.then(data => {
+console.log(data);
+})
+.catch(err => alert("Weather Not Found. Please Search Again."));
+}
 
 document.querySelector(".search-bar").addEventListener("click", function () {
-    weather.search();
+var inputValue = document.querySelector(".input").value;
+getWeather(inputValue);
 });
 
-
 // Display Weather Results
-function getWeather(inputValue) {
-    fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${inputValue}&appid=4fb00fc4e626764db50e2ea3b5fe5a50`)
-      .then(response => response.json())
-      .then(data => {
-        console.log(data);
-      })
-      .catch(err => alert("Weather Not Found. Please Search Again."));
-  }
-  
-  document.querySelector(".search-bar").addEventListener("click", function () {
-    var inputValue = document.querySelector(".input").value;
-    getWeather(inputValue);
-  });
-  
-
-form.addEventListener('click', function(){
-    fetch('https://api.openweathermap.org/data/2.5/forecast?q='+inputValue+'&appid=4fb00fc4e626764db50e2ea3b5fe5a50')
-    .then(response => response.json())
-    .then(data => console.log(data))
-
-.catch(err => alert("Weather Not Found. Please Search Again."))
+form.addEventListener('submit', function(e) {
+e.preventDefault();
+var inputValue = document.querySelector(".input").value;
+fetch(https://api.openweathermap.org/data/2.5/forecast?q=${inputValue}&appid=${APIKey}&units=metric)
+.then(response => response.json())
+.then(data => {
+console.log(data);
+// call a function to display weather data on the DOM here
 })
-
-// 
-
-let apiURL = '${queryURL}?q${city}&appid=${APIKey}&units=metric';
-
-axios.get(apiURL)
-    .then(response => {
-
-    )
-
-    function displayWeather({
-        temp: forecast,main,temp,
-        humidity: forecast,main,humidity,
-        windSpeed: forecast,wind,speed,
-        icon: forecast,weather,[0]:icon
-    }) 
-    
-    {
-        var city = response.resolvedLocation;
-        var speed = response.data.windSpeed;
-        var days = response.days;
-        var icon = response.data.weather[0];
-        console.log("Weather Found:"+tempmax);
-        for (var i = 0; i < days; i++) {
-            
-            console.log(days[i].datetime+": tempmax="+days[i].tempmax+", tempmin="+days[i].tempmin);
-        }
-    };
-;
+.catch(err => alert("Weather Not Found. Please Search Again."))
+});
 
 // Weather icons from open weather map
-var iconWeather = 'https://openweathermap.org/img/w/${weather.weather[0].icon}.png';
+var iconWeather = https://openweathermap.org/img/w/${weather.weather[0].icon}.png;
 
-
-//Store Data to Local Storage
-var searchHistory = JSON.parse(localStorage.setItem('search_history')) || [];
-if (searchHistory.indexof(inputValue) === -1)
-window.localStorage.setItem("searchHistory", JSON.stringify(searchHistory));
+// Store Data to Local Storage
+var searchHistory = JSON.parse(localStorage.getItem('search_history')) || [];
+if (searchHistory.indexOf(inputValue) === -1) {
+searchHistory.push(inputValue);
+window.localStorage.setItem("search_history", JSON.stringify(searchHistory));
+}
